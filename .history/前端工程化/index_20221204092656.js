5263a243a74402ed363299ae41bbe8215de7e83d
隔壁班的白芷 <1069936623@qq.com>
2023-02-25 13:12:45 +0800

    promis

---
+++ .history/前端工程化/index_20221204092656.js
@@ -0,0 +1,8 @@
+import m1, { s1, s2, say } from './m1'
+
+console.log(m1)
+
+
+console.log(s1)
+console.log(s2)
+console.log(say)
---
+++ .history/前端工程化/m1_20221129171248.js
@@ -0,0 +1,12 @@
+let a = 10
+let c = 20
+let d = 30
+
+function show() {
+    console.log('11111')
+}
+export default {
+    a,
+    c,
+    show
+}
